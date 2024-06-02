065d3ee247f966eabb485a71b343832f10e4f29f
Ahsin Khalid <ahsinkhan5955@gmail.com>
2024-06-02 18:07:24 +0500

    Deploy frontend to S3

---
+++ reactnd-contacts-complete/bin/deploy.ps1
@@ -0,0 +1,10 @@
+# PowerShell script to deploy build files to S3
+
+# Replace this with your actual S3 bucket name
+$s3BucketName = "cdmybucket1234"
+
+# Path to the build directory
+$buildPath = "./build"
+
+# AWS CLI command to copy files to S3 without setting ACLs
+aws s3 cp --recursive $buildPath "s3://$s3BucketName/"
--- reactnd-contacts-complete/bin/deploy.sh
+++ reactnd-contacts-complete/bin/deploy.sh
@@ -1,1 +1,2 @@
-aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+# aws s3 cp --recursive --acl public-read ./build s3://udagram-test01/
+aws s3 cp --recursive ./build s3://cdmybucket1234/
